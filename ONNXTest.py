1991be06e670090ad8e220124fb70b4ee4e5d0d2
EJO72 <eric.oneil1259@gmail.com>
2025-01-02 15:48:24 -0500

    Added parameters for the fruad check function

--- ONNXTest.py
+++ ONNXTest.py
@@ -13,7 +13,7 @@ onnx_path = "lightgbm_model.onnx"
 session = ort.InferenceSession(onnx_path)
 print('Model loaded...')
 
-input_data = pd.read_csv(r'datasets\imputed_train_data.csv', nrows=300)
+input_data = pd.read_csv(r'datasets\imputed_train_data.csv', nrows=590540)
 input_data = input_data.drop(columns=["isFraud"])
 
 input_data = scaler.transform(input_data)  # ensures this scales the same way as during training
@@ -24,13 +24,37 @@ input_name = session.get_inputs()[0].name
 outputs = session.run(None, {input_name: input_array})
 
 fraud_probabilities = outputs[1] 
-i = 1
-for fruad_probability in fraud_probabilities:
-    prob_value = ((fruad_probability[1]) * 100)
-    if prob_value > 70:
-        print(f'{i}: Probability value: {prob_value:.6f}% (LIKELY FRUAD)')
-    else: 
-        print(f'{i}: Probability value: {prob_value:.6f}%')
-    i += 1
 
-print(fraud_probabilities)
+
+def print_fruad_probabilities(probabilties, confidence_low = 50, confidence_high = 90, print_all = True):
+    i = 1
+    num_fruad_high = 0
+    num_fruad_low = 0
+    confidence_low = 50
+
+    if (print_all):
+        for probability in probabilties:
+            prob_value = ((probability[1]) * 100)
+            if prob_value > confidence_high:
+                print(f'{i}: Probability value: {prob_value:.6f}% (FRUAD [HIGH])')
+                num_fruad_high += 1
+            elif prob_value > confidence_low:
+                print(f'{i}: Probability value: {prob_value:.6f}% (FRUAD [LOW])')
+                num_fruad_low += 1
+            else: 
+                print(f'{i}: Probability value: {prob_value:.6f}%')
+            i += 1
+    else:
+        for probability in probabilties:
+            prob_value = ((probability[1]) * 100)
+            if prob_value > confidence_high:
+                print(f'{i}: Probability value: {prob_value:.6f}% (FRUAD [HIGH])')
+                num_fruad_high += 1
+            elif prob_value >= confidence_low:
+                print(f'{i}: Probability value: {prob_value:.6f}% (FRUAD [LOW])')
+                num_fruad_low += 1
+            i += 1
+    print(f'High Confidence Fruadulant Transactions: {num_fruad_high} {num_fruad_high / 500000}')
+    print(f'Low Confidence Fruadulant Transactions: {num_fruad_low} {num_fruad_low / 500000}')
+
+print_fruad_probabilities(fraud_probabilities, print_all = False)
